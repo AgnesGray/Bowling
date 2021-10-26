83e3c9ff9072bb5c6d9e14bffdbc323dfcb03ae0
miriam.tamasila <miriam.tamasila@nexxbiz.io>
2021-10-26 10:51:29 +0300

    New solution

---
+++ Bowling/BowlingLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BowlingLibrary
+{
+    public class Class1
+    {
+        public void writeInConsole()
+        {
+            Console.WriteLine("test");
+        }
+    }
+}
---
+++ Bowling/ConsoleApplication/Program.cs
@@ -0,0 +1,16 @@
+﻿using BowlingLibrary;
+using System;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 c = new Class1();
+            c.writeInConsole();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
